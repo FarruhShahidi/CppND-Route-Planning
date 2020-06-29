200630a946868f2c0accd6cb6a5c4a89160dd92c
Farruh Shahidi <farruh.shahidi@gmail.com>
2020-06-29 12:09:28 -0400

    first commit

---
+++ home/CppND-Route-Planning-Project/Astar.py
@@ -0,0 +1,12 @@
+
+
+class State():
+    def __init__(self, value, parent, start = 0 , end = 0, solver = 0):
+        self.children = []
+        self.parent = parent
+        self.value = value
+        self.dist = 0
+        if parent:
+            self.path = parent.path[:]
+            self.path.append(value)
+            self.start = parent.start
